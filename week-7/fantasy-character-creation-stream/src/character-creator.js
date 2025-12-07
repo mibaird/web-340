ba09fdb75f2e2e8a883030e76c749111bed76dfe
Michelle Baird <mibaird@365.bellevue.edu>
2025-12-07 09:19:43 -0600

    Completed week-7 assigments

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,57 @@
+"use strict";
+
+/*
+    Author: Michelle Baird
+    Date: December 5, 2025
+    File Name: character-creator.js
+    Description: fantasy-game-character test
+ */
+
 const { Duplex } = require('stream');
 
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
-    // TODO: Initialize your class here
+    // Initialize class
+    this.queue = [];
   }
 
   _write(chunk, encoding, callback) {
-    // TODO: Implement your _write method here
+    // Implement _write method
+    const input = chunk.toString().trim();
+
+    // Validate input
+    if (!input) {
+      callback(new Error("Invalid character data"));
+      return;
+    }
+
+    const parts = input.split("|");
+
+    if (parts.length < 3) {
+      callback(new Error("Invalid character data"));
+      return;
+    }
+
+    const characterClass = parts[0].trim();
+    const gender = parts[1].trim();
+    const funFact = parts.slice(2).join("|").trim();
+
+    const description =
+      `The character is a ${gender} ${characterClass}. Fun fact: ${funFact}.`;
+
+    this.queue.push(description);
+
+    this._read();
+
+    callback();
   }
 
   _read(size) {
-    // TODO: Implement your _read method here
+    if (this.queue.length > 0) {
+      this.push(this.queue.shift() + "\n");
+    } else {
+    }
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,3 +1,12 @@
+"use strict"
+
+/*
+    Author: Michelle Baird
+    Date: December 5, 2025
+    File Name: character-creator.spec.js
+    Description: fantasy-game-character test
+ */
+
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
@@ -8,14 +17,58 @@ describe('CharacterCreator', () => {
   });
 
   test("should process data correctly when written to", (done) => {
-    // TODO: Write your test here
+    const inputs = [
+      "Warrior|Male|Fights well with special sword",
+      "Mage|Female|Excels at spell work"
+    ];
+
+    const expected = [
+      'The character is a Male Warrior. Fun fact: Fights well with special sword.',
+      'The character is a Female Mage. Fun fact: Excels at spell work.'
+    ];
+
+    const outputs = [];
+
+    characterCreator.on("data", (chunk) => {
+      outputs.push(chunk.toString().trim());
+
+      // When all outputs are received, assert and finish the test
+      if (outputs.length === expected.length) {
+        expect(outputs).toEqual(expected);
+        done();
+      }
+    });
+
+    // Write all inputs into the stream, then end it
+    inputs.forEach(str => characterCreator.write(str));
+    characterCreator.end();
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
-    // TODO: Write your test here
+    // Listen for the error event
+    characterCreator.on("error", (err) => {
+      expect(err).toBeInstanceOf(Error);
+      expect(err.message).toBe("Invalid character data");
+      done();
+    });
+
+    // Write an empty string should be treated as invalid data
+    characterCreator.write("");
   });
 
   test("should transform data correctly when written to", (done) => {
-    // TODO: Write your test here
+    const input = "Rogue|Other|Prefers stealth missions";
+
+    const expected =
+      'The character is a Other Rogue. Fun fact: Prefers stealth missions.';
+
+    characterCreator.on("data", (chunk) => {
+      const output = chunk.toString().trim();
+      expect(output).toBe(expected);
+      done();
+    });
+
+    characterCreator.write(input);
+    characterCreator.end();
   });
 });
